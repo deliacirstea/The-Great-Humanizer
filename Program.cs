8df9764dfe6e249f92c8ed52805596639e58dfdf
deliacirstea <deliutacirstea@gmail.com>
2022-10-20 11:26:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Humanizer;
+using System;
+
+namespace DateTimeFormatInCSharpSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get current DateTime. It can be any DateTime object in your code.  
+            DateTime aDate = DateTime.Now;
+
+            // Format Datetime in different formats and display them  
+            Console.WriteLine("What is the time now?");
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy HH:mm:ss\n"));
+
+            Console.WriteLine("--------------------------");
+            //Console.ReadKey();
+
+            Console.WriteLine("When is the feast?");
+            Console.WriteLine($"The feast is in {DateTime.UtcNow.AddHours(15).Humanize()} ");
+            
+        }
+        
+    }
+}
